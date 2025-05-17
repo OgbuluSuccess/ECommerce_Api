1917327f06446a9e0cb102034caa8b8da9f4eb2a
Success Ogb <109591997+OgbuluSuccess@users.noreply.github.com>
2025-05-17 21:14:43 +0100

    fix_on_ss3

--- src/swagger.js
+++ src/swagger.js
@@ -10,13 +10,14 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:5000/api',
-        description: 'Local Development server',
-      },
-      {
         url: 'https://api.icedeluxewears.com',
         description: 'Production server',
       },
+      {
+        url: 'http://localhost:5000/api',
+        description: 'Local Development server',
+      },
+      
       {
         url: 'http://16.170.229.92:5000/api',
         description: 'EC2 Development server',
